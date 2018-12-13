9e1a86dc4779d06b70fddb19f47fdb9959846570
Damian Brady <info@damianbrady.com.au>
2018-12-14 02:39:44 +1100

    day 5 part 2

--- day5/Program.cs
+++ day5/Program.cs
@@ -2,45 +2,56 @@
 using System.IO;
 using System.Collections.Generic;
 using System.Text;
+using System.Collections;
 
 namespace day5
 {
     class Program
     {
-        public static string[] pairs = {"aA","bB","cC","dD","eE","fF","gG","hH","iI","jJ","kK","lL","mM","nN","oO","pP","qQ","rR","sS","tT","uU","vV","wW","xX","yY","zZ",
-                "Aa","Bb","Cc","Dd","Ee","Ff","Gg","Hh","Ii","Jj","Kk","Ll","Mm","Nn","Oo","Pp","Qq","Rr","Ss","Tt","Uu","Vv","Ww","Xx","Yy","Zz"};
+        public static string[] letters = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
 
         static void Main(string[] args)
         {
             var filename = "input.txt";
-            var bytes = System.Text.ASCIIEncoding.ASCII.GetBytes(System.IO.File.ReadAllText(filename));
-            var bytelist = new List<Byte>(bytes);
-            Console.WriteLine(ToStr(bytelist.ToArray()));
-            var i = 0;
-            var lastNon = 0;
-            for (i = 1; i < bytelist.Count; i++)
+            var input = System.IO.File.ReadAllText(filename);
+
+            var minResult = input.Length;
+            foreach (var letter in letters)
             {
-                var b1 = bytelist[i - 1];
-                var b2 = bytelist[i];
-                if (b1 - b2 == 32 || b1 - b2 == -32)
+                var newinput = new StringBuilder(input);
+                newinput.Replace(letter, "");
+                newinput.Replace(letter.ToUpper(), "");
+
+                var bytes = System.Text.ASCIIEncoding.ASCII.GetBytes(newinput.ToString());
+                var bytelist = new List<Byte>(bytes);
+                //Console.WriteLine(ToStr(bytelist.ToArray()));
+                var i = 0;
+                var lastNon = 0;
+
+                for (i = 1; i < bytelist.Count; i++)
                 {
-                    bytelist.RemoveAt(i - 1);
-                    bytelist.RemoveAt(i - 1);
-                    i = lastNon;    // backtrack - they're dead
-                    lastNon = Math.Max(lastNon - 2, 0);
-                    Console.Write($"{ToStr(b1)}{ToStr(b2)}|");
+                    var b1 = bytelist[i - 1];
+                    var b2 = bytelist[i];
+                    if (b1 - b2 == 32 || b1 - b2 == -32)
+                    {
+                        bytelist.RemoveAt(i - 1);
+                        bytelist.RemoveAt(i - 1);
+                        i = lastNon;    // backtrack - they're dead
+                        lastNon = Math.Max(lastNon - 2, 0);
+                        //Console.Write($"{ToStr(b1)}{ToStr(b2)}|");
+                    }
+                    else
+                    {
+                        lastNon = i - 1;
+                    }
                 }
-                else
-                {
-                    lastNon = i - 1;
+
+                Console.WriteLine($"Removing {letter}: {bytelist.Count}");
+                if (bytelist.Count < minResult) {
+                    Console.WriteLine("  -- new minimum");
+                    minResult = bytelist.Count;
                 }
             }
-
-            Console.WriteLine();
-            Console.WriteLine("Final result: " + ToStr(bytelist.ToArray()));
-
-            Console.WriteLine();
-            Console.WriteLine($"{bytelist.Count} units remain");
         }
 
         private static string ToStr(byte byteVal)
