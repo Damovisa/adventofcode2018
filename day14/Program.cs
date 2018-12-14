bcf978ebdd6b9b90d3d4480da0a08ec8628abde3
Damian Brady <info@damianbrady.com.au>
2018-12-14 17:57:30 +1100

    day 14 part 2 (not working)

--- day14/Program.cs
+++ day14/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace day14
 {
@@ -8,24 +9,46 @@ namespace day14
     {
         static void Main(string[] args)
         {
-            //var input = new List<int>() {7,6,8,0,7,1};
-            var input = new List<int>() { 3, 7 };
+            var start = new List<int>() { 3, 7 };
+            
+            //var s = new int[] {0,5,9,4,1,4};
+            //var s = new int[] {1,0,1,2,4,5};
+            //var s = new int[] {0,9,2,5,1,0};
+            //var s = new int[] {7,6,8,0,7,1};
+            //var s = new int[] {1,6,7,7,9,2};
+
+            var s = new int[] {7,9,3,0,6,1};
 
             var e1 = 0;
             var e2 = 1;
-            // loop until we hit the required number
-            while (input.Count<=768071 + 10)
+            // loop until we find it
+            var found = false;
+            while (!found)
             {
                 // new recipes added
-                var sumdigits = (input[e1] + input[e2]).ToString().Select(c => int.Parse(c.ToString()));
-                input.AddRange(sumdigits);
+                var sumdigits = (start[e1] + start[e2]).ToString().Select(c => int.Parse(c.ToString()));
+                start.AddRange(sumdigits);
 
                 // pick new recipe
-                e1 = (e1 + input[e1] + 1) % input.Count;
-                e2 = (e2 + input[e2] + 1) % input.Count;
+                e1 = (e1 + start[e1] + 1) % start.Count;
+                e2 = (e2 + start[e2] + 1) % start.Count;
+
+                if (start.Count>5) {
+                    var len = start.Count;
+                    found = (
+                        (start[len-6] == s[0]) &&
+                        (start[len-5] == s[1]) &&
+                        (start[len-4] == s[2]) &&
+                        (start[len-3] == s[3]) &&
+                        (start[len-2] == s[4]) &&
+                        (start[len-1] == s[5]));
+                }
+
             }
-            Console.WriteLine(input.Count);
-            foreach (var num in input)
+
+            Console.WriteLine($"There are {start.Count} elements");
+            Console.WriteLine(start.Count-6);
+            foreach (var num in start.Skip(start.Count-10))
             {
                 Console.Write(num);
             }
